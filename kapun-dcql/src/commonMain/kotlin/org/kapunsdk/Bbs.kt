heidiverse/heidi-sdk

adjust to new api

heidi-dcql/src/commonMain/kotlin/ch/ubique/heidi/dcql/Bbs.kt
@@ -26,11 +26,21 @@ import ch.ubique.heidi.credentials.SdJwtErrors
 import ch.ubique.heidi.util.extensions.asObject
 import ch.ubique.heidi.util.extensions.get
 import uniffi.heidi_credentials_rust.BbsRust
+import uniffi.heidi_credentials_rust.BbsWrapper
 import uniffi.heidi_credentials_rust.ClaimBasedParams
 import uniffi.heidi_credentials_rust.PointerPart
+import uniffi.heidi_credentials_rust.Selector
 import uniffi.heidi_credentials_rust.bbsDeriveClaimBasedProof
 import uniffi.heidi_credentials_rust.bbsGetBody
+import uniffi.heidi_credentials_rust.decodeBbs
+import uniffi.heidi_dcql_rust.CombinedBbsMetaMismatch
+import uniffi.heidi_dcql_rust.Credential
+import uniffi.heidi_dcql_rust.CredentialLike
+import uniffi.heidi_dcql_rust.CredentialParser
 import uniffi.heidi_dcql_rust.CredentialQuery
+import uniffi.heidi_dcql_rust.Meta
+import uniffi.heidi_dcql_rust.MetaMismatch
+import uniffi.heidi_dcql_rust.registerParser
 import uniffi.heidi_util_rust.Value
 
 sealed interface BbsErrors {
@@ -49,6 +59,56 @@ sealed interface BbsErrors {
     data object UnsatisfiableCredentialQuery : BbsErrors,
         Throwable("The credential query couldn't be satisfied")
 }
+
+object BbsParser: CredentialParser {
+    init {
+        registerParser(BbsParser)
+    }
+
+    override fun id(): String {
+        return "BBS-PARSER"
+    }
+
+    override fun fromStr(credential: String): Credential? {
+        return runCatching {
+            val bbs = decodeBbs(credential)
+            return Credential.BbsCredential(BbsCredential(BbsWrapper.fromBbs(bbs)))
+        }.getOrNull()
+    }
+
+}
+
+class BbsCredential(val bbs: BbsWrapper) : CredentialLike {
+    override fun getBody(): Value {
+       return bbs.body()
+    }
+    override fun serialize(): String {
+        return bbs.getBbs().originalBbs
+    }
+
+    override fun formatSpecifiers(): List<String> {
+        return listOf("bbs-termwise")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+        return when(meta) {
+		   is Meta.W3c -> {
+               val bbsTypes = bbs.types()
+               return if(!meta.credentialTypes.any { bbsTypes.contains(it) }) {
+                   MetaMismatch.BbsMetaMismatch(CombinedBbsMetaMismatch.WRONG_CREDENTIAL_TYPE)
+               } else {
+                   null
+               }
+           }
+            else if meta == null -> null
+            else -> MetaMismatch.BbsMetaMismatch(CombinedBbsMetaMismatch.INVALID_META)
+	   }
+    }
+    override fun get(selector: Selector): List<Value>? {
+        return bbs.get(selector)
+    }
+}
+
 
 fun BbsRust.body() : Value {
     return bbsGetBody(this)

heidi-dcql/src/commonMain/kotlin/ch/ubique/heidi/dcql/Mdoc.kt
@@ -23,6 +23,8 @@ package ch.ubique.heidi.dcql
 import ch.ubique.heidi.credentials.Mdoc
 import ch.ubique.heidi.credentials.Mdoc.Companion.MDOC_FORMATS
 import ch.ubique.heidi.credentials.SdJwtErrors
+import ch.ubique.heidi.credentials.get
+import ch.ubique.heidi.credentials.toClaimsPointer
 import ch.ubique.heidi.util.extensions.asArray
 import ch.ubique.heidi.util.extensions.asBytes
 import ch.ubique.heidi.util.extensions.asObject
@@ -32,13 +34,80 @@ import ch.ubique.heidi.util.extensions.asTag
 import ch.ubique.heidi.util.extensions.get
 import ch.ubique.heidi.util.extensions.isSame
 import ch.ubique.heidi.util.extensions.toCbor
+import kotlinx.serialization.json.Json
+import uniffi.heidi_credentials_rust.MdocRust
 import uniffi.heidi_credentials_rust.PointerPart
+import uniffi.heidi_credentials_rust.Selector
 import uniffi.heidi_credentials_rust.SignatureCreator
+import uniffi.heidi_credentials_rust.decodeMdoc
 import uniffi.heidi_crypto_rust.base64UrlEncode
+import uniffi.heidi_dcql_rust.CombinedMdocMetaMismatch
+import uniffi.heidi_dcql_rust.Credential
+import uniffi.heidi_dcql_rust.CredentialLike
+import uniffi.heidi_dcql_rust.CredentialParser
 import uniffi.heidi_dcql_rust.CredentialQuery
+import uniffi.heidi_dcql_rust.Meta
+import uniffi.heidi_dcql_rust.MetaMismatch
+import uniffi.heidi_dcql_rust.registerParser
 import uniffi.heidi_util_rust.Value
 import uniffi.heidi_util_rust.decodeCbor
 import uniffi.heidi_util_rust.encodeCbor
+
+object MdocParser: CredentialParser {
+    init {
+        registerParser(this)
+    }
+    override fun id(): String {
+        return "mdoc-parser"
+    }
+
+    override fun fromStr(credential: String): Credential? {
+        return runCatching { decodeMdoc(credential) }.getOrNull()?.let {
+            Credential.MdocCredential(MdocCredential(it))
+        }
+    }
+}
+
+class MdocCredential(val mdoc: MdocRust) : CredentialLike {
+    override fun getBody(): Value {
+        return mdoc.namespaceMap
+    }
+
+    override fun serialize(): String {
+        return mdoc.originalMdoc
+    }
+
+    override fun formatSpecifiers(): List<String> {
+       return listOf("mso_mdoc")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+       return when(meta){
+		   is Meta.IsoMdoc -> {
+               val docType = docType() ?: return MetaMismatch.MdocMetaMismatch(CombinedMdocMetaMismatch.WRONG_DOC_TYPE)
+               return if(docType != meta.doctypeValue) {
+                   MetaMismatch.MdocMetaMismatch(CombinedMdocMetaMismatch.WRONG_DOC_TYPE)
+               } else {
+                   null
+               }
+           }
+		   null -> null
+           else -> MetaMismatch.MdocMetaMismatch(CombinedMdocMetaMismatch.INVALID_META)
+	   }
+    }
+
+    override fun get(selector: Selector): List<Value>? {
+		return mdoc.namespaceMap[selector]
+    }
+    fun docType() : String? {
+        val result = this.mdoc.issuerAuth[listOf("docType").toClaimsPointer()!!]
+        if (result.size != 1) {
+            return null
+        }
+        return result[0].asString()
+    }
+}
+
 
 fun Mdoc.getVpToken(
     query: CredentialQuery,

heidi-dcql/src/commonMain/kotlin/ch/ubique/heidi/dcql/SdJwtGeneric.kt
@@ -22,12 +22,104 @@ package ch.ubique.heidi.dcql
 
 import uniffi.heidi_dcql_rust.CredentialQuery
 import ch.ubique.heidi.credentials.*
+import ch.ubique.heidi.util.extensions.asString
+import ch.ubique.heidi.util.extensions.get
+import kotlinx.serialization.json.Json
 import uniffi.heidi_credentials_rust.*
+import uniffi.heidi_dcql_rust.CombinedSdJwtMetaMismatch
+import uniffi.heidi_dcql_rust.Credential
+import uniffi.heidi_dcql_rust.CredentialLike
+import uniffi.heidi_dcql_rust.CredentialParser
+import uniffi.heidi_dcql_rust.Meta
+import uniffi.heidi_dcql_rust.MetaMismatch
+import uniffi.heidi_dcql_rust.registerParser
 import uniffi.heidi_util_rust.*
+
+object SdJwtW3CParser: CredentialParser {
+
+    init {
+        registerParser(this)
+    }
+
+    override fun id(): String {
+        return "sdjwt+w3c-parser"
+    }
+
+    override fun fromStr(credential: String): Credential? {
+        val sdjwt = runCatching { decodeSdjwt(credential) }.getOrNull()
+        val w3c = runCatching { parseW3cSdJwt(credential) }.getOrNull()
+        return if(sdjwt != null && w3c != null) {
+            if(w3c.json["@context"] != Value.Null) {
+                Credential.W3cCredential(W3CCredential(w3c = w3c))
+            } else {
+                Credential.SdJwtCredential(SdJwtCredential(sdjwt = sdjwt))
+            }
+        } else if (sdjwt != null) {
+            Credential.SdJwtCredential(SdJwtCredential(sdjwt = sdjwt))
+        } else if (w3c != null) {
+            Credential.W3cCredential(W3CCredential(w3c = w3c))
+        } else {
+            null
+        }
+    }
+}
+
+class SdJwtCredential(val sdjwt: SdJwtRust): CredentialLike {
+    override fun getBody(): Value {
+        return sdjwt.claims
+    }
+
+    override fun serialize(): String {
+        return sdjwt.originalSdjwt
+    }
+
+    override fun formatSpecifiers(): List<String> {
+       return listOf("dc+sd-jwt", "vc+sd-jwt")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+        val vct = this.sdjwt.claims["vct"].asString() ?: return MetaMismatch.SdJwtMetaMismatch(CombinedSdJwtMetaMismatch.WRONG_VCT_VALUE)
+        return when(meta) {
+            is Meta.SdjwtVc -> {
+                if(meta.vctValues.any { vct == it }){
+                    null
+                } else {
+                    MetaMismatch.SdJwtMetaMismatch(CombinedSdJwtMetaMismatch.WRONG_VCT_VALUE)
+                }
+            }
+            else if meta == null -> null
+            else -> MetaMismatch.SdJwtMetaMismatch(CombinedSdJwtMetaMismatch.INVALID_META)
+        }
+    }
+
+    override fun get(selector: Selector): List<Value>? {
+        return sdjwt.claims[selector]
+    }
+}
+class W3CCredential(val w3c: W3cSdJwt): CredentialLike {
+    override fun getBody(): Value {
+        return w3c.json
+    }
+
+    override fun serialize(): String {
+        return Json{}.encodeToString(w3c)
+    }
+
+    override fun formatSpecifiers(): List<String> {
+        return listOf("vc+sd-jwt")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+        return null
+    }
+
+    override fun get(selector: Selector): List<Value>? {
+        return w3c.json[selector]
+    }
+}
 
 fun SdJwtBuilder.getVpToken(
     claims: Value,
-
     query: CredentialQuery,
     audience: String,
     transactionData: List<String>?,

heidi-dcql/src/commonMain/kotlin/ch/ubique/heidi/dcql/trustedAuthority/AkiAuthorityMatcher.kt
@@ -1,5 +1,6 @@
 package ch.ubique.heidi.dcql.trustedAuthority
 
+import uniffi.heidi_credentials_rust.decodeSdjwt
 import uniffi.heidi_crypto_rust.getX509FromJwt
 import uniffi.heidi_dcql_rust.Credential
 import uniffi.heidi_dcql_rust.TrustedAuthority
@@ -21,7 +22,8 @@ object AkiAuthorityMatcher : TrustedAuthorityMatcher {
 		if(value !is Credential.SdJwtCredential) {
 			return null
 		}
-		val x509Chain = getX509FromJwt(value.v1.originalJwt) ?: return null
+		val v = decodeSdjwt(value.v1.serialize())
+		val x509Chain = getX509FromJwt(v.originalJwt) ?: return null
 		for(c in x509Chain) {
 			if(trustedAuthority.values.contains(c.authorityKeyIdentifier)) {
 				return true

heidi-dcql/src/commonMain/kotlin/ch/ubique/heidi/dcql/trustedAuthority/DidAuthorityMatcher.kt
@@ -2,6 +2,7 @@ package ch.ubique.heidi.dcql.trustedAuthority
 
 import ch.ubique.heidi.util.extensions.asString
 import ch.ubique.heidi.util.extensions.get
+import uniffi.heidi_credentials_rust.decodeSdjwt
 import uniffi.heidi_crypto_rust.getKidFromJwt
 import uniffi.heidi_dcql_rust.Credential
 import uniffi.heidi_dcql_rust.TrustedAuthority
@@ -19,9 +20,10 @@ object DidAuthorityMatcher : TrustedAuthorityMatcher {
 	override fun matches(value: Credential, trustedAuthority: TrustedAuthority): Boolean? {
 		return when (value) {
 			is Credential.SdJwtCredential -> {
-				val kid = getKidFromJwt(value.v1.originalJwt)
+				val jwt = decodeSdjwt(value.v1.serialize())
+				val kid = getKidFromJwt(jwt.originalJwt)
 				val did = kid?.split("#")?.firstOrNull()
-				val issuer = value.v1.claims["iss"].asString() ?: did ?: return false
+				val issuer = jwt.claims["iss"].asString() ?: did ?: return false
 				trustedAuthority.values.contains(issuer)
 			}
 			else -> null

KapunSDK/kapun-sdk

kapun-dcql/src/commonMain/kotlin/org/kapunsdk/Bbs.kt
@@ -26,12 +26,22 @@ import org.kapunsdk.credentials.SdJwtErrors
 import org.kapunsdk.util.extensions.asObject
 import org.kapunsdk.util.extensions.get
 import uniffi.kapun_credentials_rust.BbsRust
+import uniffi.kapun_credentials_rust.BbsWrapper
 import uniffi.kapun_credentials_rust.ClaimBasedParams
 import uniffi.kapun_credentials_rust.DeviceBindingType
 import uniffi.kapun_credentials_rust.PointerPart
+import uniffi.kapun_credentials_rust.Selector
 import uniffi.kapun_credentials_rust.bbsDeriveClaimBasedProof
 import uniffi.kapun_credentials_rust.bbsGetBody
+import uniffi.kapun_credentials_rust.decodeBbs
+import uniffi.kapun_dcql_rust.CombinedBbsMetaMismatch
+import uniffi.kapun_dcql_rust.Credential
+import uniffi.kapun_dcql_rust.CredentialLike
+import uniffi.kapun_dcql_rust.CredentialParser
 import uniffi.kapun_dcql_rust.CredentialQuery
+import uniffi.kapun_dcql_rust.Meta
+import uniffi.kapun_dcql_rust.MetaMismatch
+import uniffi.kapun_dcql_rust.registerParser
 import uniffi.kapun_util_rust.Value
 import kotlin.text.iterator
 
@@ -51,6 +61,56 @@ sealed interface BbsErrors {
     data object UnsatisfiableCredentialQuery : BbsErrors,
         Throwable("The credential query couldn't be satisfied")
 }
+
+object BbsParser: CredentialParser {
+    init {
+        registerParser(BbsParser)
+    }
+
+    override fun id(): String {
+        return "BBS-PARSER"
+    }
+
+    override fun fromStr(credential: String): Credential? {
+        return runCatching {
+            val bbs = decodeBbs(credential)
+            return Credential.BbsCredential(BbsCredential(BbsWrapper.fromBbs(bbs)))
+        }.getOrNull()
+    }
+
+}
+
+class BbsCredential(val bbs: BbsWrapper) : CredentialLike {
+    override fun getBody(): Value {
+       return bbs.body()
+    }
+    override fun serialize(): String {
+        return bbs.getBbs().originalBbs
+    }
+
+    override fun formatSpecifiers(): List<String> {
+        return listOf("bbs-termwise")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+        return when(meta) {
+		   is Meta.W3c -> {
+               val bbsTypes = bbs.types()
+               return if(!meta.credentialTypes.any { bbsTypes.contains(it) }) {
+                   MetaMismatch.BbsMetaMismatch(CombinedBbsMetaMismatch.WRONG_CREDENTIAL_TYPE)
+               } else {
+                   null
+               }
+           }
+            else if meta == null -> null
+            else -> MetaMismatch.BbsMetaMismatch(CombinedBbsMetaMismatch.INVALID_META)
+	   }
+    }
+    override fun get(selector: Selector): List<Value>? {
+        return bbs.get(selector)
+    }
+}
+
 
 fun BbsRust.body() : Value {
     return bbsGetBody(this)

kapun-dcql/src/commonMain/kotlin/org/kapunsdk/Mdoc.kt
@@ -23,6 +23,8 @@ package org.kapunsdk
 import org.kapunsdk.credentials.Mdoc
 import org.kapunsdk.credentials.Mdoc.Companion.MDOC_FORMATS
 import org.kapunsdk.credentials.SdJwtErrors
+import org.kapunsdk.credentials.get
+import org.kapunsdk.credentials.toClaimsPointer
 import org.kapunsdk.util.extensions.asArray
 import org.kapunsdk.util.extensions.asBytes
 import org.kapunsdk.util.extensions.asObject
@@ -32,15 +34,82 @@ import org.kapunsdk.util.extensions.asTag
 import org.kapunsdk.util.extensions.get
 import org.kapunsdk.util.extensions.isSame
 import org.kapunsdk.util.extensions.toCbor
+import kotlinx.serialization.json.Json
+import uniffi.kapun_credentials_rust.MdocRust
 import uniffi.kapun_credentials_rust.PointerPart
+import uniffi.kapun_credentials_rust.Selector
 import uniffi.kapun_credentials_rust.SignatureCreator
+import uniffi.kapun_credentials_rust.decodeMdoc
 import uniffi.kapun_crypto_rust.base64UrlEncode
+import uniffi.kapun_dcql_rust.CombinedMdocMetaMismatch
+import uniffi.kapun_dcql_rust.Credential
+import uniffi.kapun_dcql_rust.CredentialLike
+import uniffi.kapun_dcql_rust.CredentialParser
 import uniffi.kapun_dcql_rust.CredentialQuery
+import uniffi.kapun_dcql_rust.Meta
+import uniffi.kapun_dcql_rust.MetaMismatch
+import uniffi.kapun_dcql_rust.registerParser
 import uniffi.kapun_util_rust.Value
 import uniffi.kapun_util_rust.decodeCbor
 import uniffi.kapun_util_rust.encodeCbor
 import kotlin.collections.iterator
 import kotlin.text.iterator
+
+object MdocParser: CredentialParser {
+    init {
+        registerParser(this)
+    }
+    override fun id(): String {
+        return "mdoc-parser"
+    }
+
+    override fun fromStr(credential: String): Credential? {
+        return runCatching { decodeMdoc(credential) }.getOrNull()?.let {
+            Credential.MdocCredential(MdocCredential(it))
+        }
+    }
+}
+
+class MdocCredential(val mdoc: MdocRust) : CredentialLike {
+    override fun getBody(): Value {
+        return mdoc.namespaceMap
+    }
+
+    override fun serialize(): String {
+        return mdoc.originalMdoc
+    }
+
+    override fun formatSpecifiers(): List<String> {
+       return listOf("mso_mdoc")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+       return when(meta){
+		   is Meta.IsoMdoc -> {
+               val docType = docType() ?: return MetaMismatch.MdocMetaMismatch(CombinedMdocMetaMismatch.WRONG_DOC_TYPE)
+               return if(docType != meta.doctypeValue) {
+                   MetaMismatch.MdocMetaMismatch(CombinedMdocMetaMismatch.WRONG_DOC_TYPE)
+               } else {
+                   null
+               }
+           }
+		   null -> null
+           else -> MetaMismatch.MdocMetaMismatch(CombinedMdocMetaMismatch.INVALID_META)
+	   }
+    }
+
+    override fun get(selector: Selector): List<Value>? {
+		return mdoc.namespaceMap[selector]
+    }
+    fun docType() : String? {
+        val result = this.mdoc.issuerAuth[listOf("docType").toClaimsPointer()!!]
+        if (result.size != 1) {
+            return null
+        }
+        return result[0].asString()
+    }
+}
+
 
 fun Mdoc.getVpToken(
     query: CredentialQuery,

kapun-dcql/src/commonMain/kotlin/org/kapunsdk/SdJwtGeneric.kt
@@ -20,15 +20,107 @@ under the License.
 
 package org.kapunsdk
 
-import uniffi.kapun_dcql_rust.CredentialQuery
 import org.kapunsdk.credentials.*
+import org.kapunsdk.util.extensions.asString
+import org.kapunsdk.util.extensions.get
+import kotlinx.serialization.json.Json
 import uniffi.kapun_credentials_rust.*
+import uniffi.kapun_dcql_rust.CombinedSdJwtMetaMismatch
+import uniffi.kapun_dcql_rust.Credential
+import uniffi.kapun_dcql_rust.CredentialLike
+import uniffi.kapun_dcql_rust.CredentialParser
+import uniffi.kapun_dcql_rust.CredentialQuery
+import uniffi.kapun_dcql_rust.Meta
+import uniffi.kapun_dcql_rust.MetaMismatch
+import uniffi.kapun_dcql_rust.registerParser
 import uniffi.kapun_util_rust.*
 import kotlin.text.iterator
 
+object SdJwtW3CParser: CredentialParser {
+
+    init {
+        registerParser(this)
+    }
+
+    override fun id(): String {
+        return "sdjwt+w3c-parser"
+    }
+
+    override fun fromStr(credential: String): Credential? {
+        val sdjwt = runCatching { decodeSdjwt(credential) }.getOrNull()
+        val w3c = runCatching { parseW3cSdJwt(credential) }.getOrNull()
+        return if(sdjwt != null && w3c != null) {
+            if(w3c.json["@context"] != Value.Null) {
+                Credential.W3cCredential(W3CCredential(w3c = w3c))
+            } else {
+                Credential.SdJwtCredential(SdJwtCredential(sdjwt = sdjwt))
+            }
+        } else if (sdjwt != null) {
+            Credential.SdJwtCredential(SdJwtCredential(sdjwt = sdjwt))
+        } else if (w3c != null) {
+            Credential.W3cCredential(W3CCredential(w3c = w3c))
+        } else {
+            null
+        }
+    }
+}
+
+class SdJwtCredential(val sdjwt: SdJwtRust): CredentialLike {
+    override fun getBody(): Value {
+        return sdjwt.claims
+    }
+
+    override fun serialize(): String {
+        return sdjwt.originalSdjwt
+    }
+
+    override fun formatSpecifiers(): List<String> {
+       return listOf("dc+sd-jwt", "vc+sd-jwt")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+        val vct = this.sdjwt.claims["vct"].asString() ?: return MetaMismatch.SdJwtMetaMismatch(CombinedSdJwtMetaMismatch.WRONG_VCT_VALUE)
+        return when(meta) {
+            is Meta.SdjwtVc -> {
+                if(meta.vctValues.any { vct == it }){
+                    null
+                } else {
+                    MetaMismatch.SdJwtMetaMismatch(CombinedSdJwtMetaMismatch.WRONG_VCT_VALUE)
+                }
+            }
+            else if meta == null -> null
+            else -> MetaMismatch.SdJwtMetaMismatch(CombinedSdJwtMetaMismatch.INVALID_META)
+        }
+    }
+
+    override fun get(selector: Selector): List<Value>? {
+        return sdjwt.claims[selector]
+    }
+}
+class W3CCredential(val w3c: W3cSdJwt): CredentialLike {
+    override fun getBody(): Value {
+        return w3c.json
+    }
+
+    override fun serialize(): String {
+        return Json{}.encodeToString(w3c)
+    }
+
+    override fun formatSpecifiers(): List<String> {
+        return listOf("vc+sd-jwt")
+    }
+
+    override fun matchesMeta(meta: Meta?): MetaMismatch? {
+        return null
+    }
+
+    override fun get(selector: Selector): List<Value>? {
+        return w3c.json[selector]
+    }
+}
+
 fun SdJwtBuilder.getVpToken(
     claims: Value,
-
     query: CredentialQuery,
     audience: String,
     transactionData: List<String>?,

kapun-dcql/src/commonMain/kotlin/org/kapunsdk/trustedAuthority/AkiAuthorityMatcher.kt
@@ -1,5 +1,6 @@
 package org.kapunsdk.trustedAuthority
 
+import uniffi.kapun_credentials_rust.decodeSdjwt
 import uniffi.kapun_crypto_rust.getX509FromJwt
 import uniffi.kapun_dcql_rust.Credential
 import uniffi.kapun_dcql_rust.TrustedAuthority
@@ -22,7 +23,8 @@ object AkiAuthorityMatcher : TrustedAuthorityMatcher {
 		if(value !is Credential.SdJwtCredential) {
 			return null
 		}
-		val x509Chain = getX509FromJwt(value.v1.originalJwt) ?: return null
+		val v = decodeSdjwt(value.v1.serialize())
+		val x509Chain = getX509FromJwt(v.originalJwt) ?: return null
 		for(c in x509Chain) {
 			if(trustedAuthority.values.contains(c.authorityKeyIdentifier)) {
 				return true

kapun-dcql/src/commonMain/kotlin/org/kapunsdk/trustedAuthority/DidAuthorityMatcher.kt
@@ -2,6 +2,7 @@ package org.kapunsdk.trustedAuthority
 
 import org.kapunsdk.util.extensions.asString
 import org.kapunsdk.util.extensions.get
+import uniffi.kapun_credentials_rust.decodeSdjwt
 import uniffi.kapun_crypto_rust.getKidFromJwt
 import uniffi.kapun_dcql_rust.Credential
 import uniffi.kapun_dcql_rust.TrustedAuthority
@@ -19,9 +20,10 @@ object DidAuthorityMatcher : TrustedAuthorityMatcher {
 	override fun matches(value: Credential, trustedAuthority: TrustedAuthority): Boolean? {
 		return when (value) {
 			is Credential.SdJwtCredential -> {
-				val kid = getKidFromJwt(value.v1.originalJwt)
+				val jwt = decodeSdjwt(value.v1.serialize())
+				val kid = getKidFromJwt(jwt.originalJwt)
 				val did = kid?.split("#")?.firstOrNull()
-				val issuer = value.v1.claims["iss"].asString() ?: did ?: return false
+				val issuer = jwt.claims["iss"].asString() ?: did ?: return false
 				trustedAuthority.values.contains(issuer)
 			}
 			else -> null